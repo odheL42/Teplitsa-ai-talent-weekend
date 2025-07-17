0e5b0c12a61e9c9bfcc2736e57fcd330278c6619
Syrenny <syrennyilya@yandex.ru>
2025-07-18 04:16:30 +0700

    Add small cosmetic improvements

--- teplitsa-recommender/frontend/src/components/Cart.tsx
+++ teplitsa-recommender/frontend/src/components/Cart.tsx
@@ -18,7 +18,6 @@ const Cart: React.FC = () => {
 	return (
 		<div>
 			<div
-				onClick={() => setIsOpen(true)}
 				className={`flex flex=cp hover:cursor-pointer ${
 					highlight ? 'animate-[ping_0.3s]' : ''
 				}`}
@@ -26,7 +25,7 @@ const Cart: React.FC = () => {
 				<button
 					className='relative transition transform hover:scale-110 active:scale-95 hover:cursor-pointer'
 					aria-label='Корзина'
-					onClick={() => console.log('Открыть корзину')}
+					onClick={() => setIsOpen(true)}
 				>
 					<ShoppingCart className='w-6 h-6 text-gray-900 dark:text-neutral-400 hover:text-green-500' />
 				</button>
--- teplitsa-recommender/frontend/src/components/PreferencesButton.tsx
+++ teplitsa-recommender/frontend/src/components/PreferencesButton.tsx
@@ -1,12 +1,12 @@
-import { useModal } from '../context/ModalContext'
 import { SlidersVertical } from 'lucide-react'
+import { useModal } from '../context/ModalContext'
 
 export const PreferencesButton = () => {
 	const { open } = useModal()
 
 	return (
 		<button onClick={open} aria-label='Открыть настройки предпочтений'>
-			<SlidersVertical className='hover:bg-gray-200 dark:hover:bg-gray-700 h-6 w-6' />
+			<SlidersVertical className=' h-6 w-6 transition-transform hover:scale-110 active:scale-95 disabled:opacity-60 hover:cursor-pointer' />
 		</button>
 	)
 }
